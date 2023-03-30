8b2cf6f34ca39774b79b78c61d99943d071b8a1b
Jagjit Singh <jagik22@gmail.com>
2023-03-30 16:05:28 -0700

    course model and seeder

---
+++ models/Course.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const CourseSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    trim: true,
+    required: [true, "Please add a course title"],
+  },
+  description: {
+    type: String,
+    required: [true, "Please add a description"],
+  },
+  weeks: {
+    type: String,
+    required: [true, "Please add number of week"],
+  },
+  tuition: {
+    type: Number,
+    required: [true, "Please add a tuition cost"],
+  },
+  minimumSkill: {
+    type: String,
+    required: [true, "Please add a minimum skill"],
+    // has to be one of these values
+    enum: ["beginner", "intermediate", "advanced"],
+  },
+  scholarshipAvailable: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  // relation
+  bootcamp: {
+    type: mongoose.Schema.ObjectId,
+    // reference Bootcamp model
+    ref: "Bootcamp",
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Course", CourseSchema);
--- seeder.js
+++ seeder.js
@@ -8,6 +8,7 @@ dotenv.config({ path: "./config/config.env" });
 
 // Load models
 const Bootcamp = require("./models/Bootcamp");
+const Course = require("./models/Course");
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -22,11 +23,15 @@ mongoose.connect(process.env.MONGO_URI, {
 const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
 );
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
+);
 
 // Import into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);
 
     console.log("Data Imported...".green.inverse);
     process.exit();
@@ -40,6 +45,7 @@ const deleteData = async () => {
   try {
     // mongoose method
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
 
     console.log("Data Destroyed...".red.inverse);
     process.exit();
